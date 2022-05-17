966b70271b0e6ca493537c5646d0a3ff3be9ea04
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-05-17 19:24:12 +0700

    add code in programe 2

---
+++ program_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int ob(int a)
+{
+    int number1=a/100;
+    //Console.WriteLine(number1);
+    int number2=a%10;
+    //Console.WriteLine(number2);
+    return 10*number1+number2;
+}
+
+int randomNumber=new Random().Next(100,1000);
+Console.WriteLine(randomNumber);
+Console.WriteLine(ob(randomNumber));
