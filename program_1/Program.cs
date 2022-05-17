a3751050bc87340c4f73024434ebdac808912bec
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-05-17 19:06:23 +0700

    add code in programe 1

---
+++ program_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int secondNumber(int number)
+{
+    return number/10%10;
+}
+string str;
+int a;
+Console.WriteLine("Введите число");
+str = Console.ReadLine();
+a=int.Parse(str);
+Console.WriteLine("вторая цифра числа "+a+": "+secondNumber(a));
+
