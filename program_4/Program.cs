99c39bccfbd427cb85e4d451f78a512ccd616dda
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-05-21 14:26:33 +0700

    add code in programe 4

---
+++ program_4/Program.cs
@@ -0,0 +1,18 @@
+﻿void week(int day)
+{
+    day=day%7;
+    if ((day==6)||(day==7))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+string str;
+int a;
+Console.WriteLine("Введите число");
+str = Console.ReadLine();
+a=int.Parse(str);
+week(a);
