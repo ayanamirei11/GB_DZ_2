df8cc48d4a6f9aba74ad8d1376998df21c841e57
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-05-21 14:26:26 +0700

    add code in programe 3

---
+++ program_3/Program.cs
@@ -0,0 +1,33 @@
+﻿int pr(int a)
+{
+    if (a>999)
+    {
+        while (a>999)
+        {
+            a/=10;
+        }
+    }
+    if ((a<999)&&(a>100))
+    {
+        return a%10;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+string str;
+int a,c;
+Console.WriteLine("Введите число");
+str = Console.ReadLine();
+a=int.Parse(str);
+c=pr(a);
+if (c==-1)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else 
+{
+    Console.WriteLine("третья цифра числа "+a+": "+c);
+}
